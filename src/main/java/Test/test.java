e747edcd385b364ce6b4805921e95c4dd18fa934
sony <sony@sony-VAIO>
2019-02-10 22:41:41 +0530

    Commit 2

---
+++ src/main/java/Test/test.java
@@ -0,0 +1,14 @@
+package Test;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class test {
+	
+	@Test()
+	public void test1()
+	{
+		Reporter.log("hi....",true);
+	}
+
+}
